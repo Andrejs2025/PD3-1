2d4156b4b7b14debf9743e83b9b4eaa1393186f1
Andrejs2025 <krotmotorbaz@gmail.com>
2026-03-19 08:48:13 +0200

    Add files via upload

--- 5_programmatura.ps1
+++ 5_programmatura.ps1
@@ -1,3 +1,15 @@
-$list = winget upgrade | Select-Object -Skip 2
-$count = ($list | Measure-Object).Count
-Write-Host "Sistēmā ir $count programmas, kuras nepieciešams atjaunināt."
+# Run the winget list command and filter for upgrade-available packages
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$updates = winget list --upgrade-available | Select-Object -Skip 3 | Where-Object { $_ -match '\S' }
+
+# Count the number of updates found
+$updateCount = $updates.Count
+
+# Output the result
+Write-Output "There are [$updateCount] programs on the system that need to be updated."
+
+# Optional: Output the list of programs with updates
+if ($updateCount -gt 0) {
+    Write-Output "`nPrograms with available updates:"
+    $updates
+}
